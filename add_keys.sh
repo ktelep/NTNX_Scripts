0778aede1d7344ba2e34102db6107715bfe683f0
Kurt Telep <ktelep@gmail.com>
2024-06-03 16:12:00 -0400

    Added add_keys.sh script

---
+++ add_keys.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Quick script to take a directory of public key files named "Key_name.pub"
+# and add them as public keys for authentication on a Prism Element cluster
+# This is useful when rotating a number of keys, note it does not REMOVE 
+# any keys from the configuration
+#
+# Keys can be any compatible type and length
+# A datecode is added to each name given
+# 
+# Ex.
+#   Filename - Kurt_Telep.pub
+#   Current Date - June 3, 2024
+#   Key Name Generated - Kurt_Telep_20240603
+#
+
+# Set to the directory where your public keys currently reside on the CVM
+KEY_PATH=/tmp/keys
+
+DATECODE=`date +%Y%m%d`
+
+for i in $(ls $KEY_PATH/*.pub)
+do
+	# Extract the name from the filename itself
+	filename=$(basename -- "$i")
+	filename="${filename%.*}"
+
+	KEYNAME="${filename}_${DATECODE}"
+	echo "Adding Key ${KEYNAME}"
+	ncli cluster add-public-key name=${KEYNAME} file-path=${i}
+done
+
